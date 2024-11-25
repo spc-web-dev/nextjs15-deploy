932f7f70515fc8cd792a994cccbabf892a2da424
spc-web-dev <itspc2023@gmail.com>
2024-11-25 17:02:39 +0700

    install shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 
+import { Button } from "@/components/ui/button";
 import { AiFillAccountBook } from "react-icons/ai";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-screen w-full grid place-items-center">
       hello guys chill
       <AiFillAccountBook />
+      <Button>Click me</Button>
     </div>
   );
 }
